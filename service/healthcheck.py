2c3edba9867e1fa66cd8a6560d75d7ab4b38dd96
prettycities <mrcyze@gmail.com>
2018-06-19 07:53:40 -0500

    Changed content type of response

--- service/healthcheck.py
+++ service/healthcheck.py
@@ -1,3 +1,4 @@
+from falcon import MEDIA_TEXT
 from falcon.response import Response
 from falcon.request import Request
 
@@ -5,3 +6,4 @@ from falcon.request import Request
 class HealthCheck(object):
     def on_get(self, req: Request, res: Response):
         res.body = "All good!"
+        res.content_type = MEDIA_TEXT
